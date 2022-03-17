d520a7e490eeee494a9034e82c83aad29117bd9a
Samaila Chatto Bashir <chattobashir@gmail.com>
2022-03-17 13:46:53 +0100

    Update entities.ts

--- src/types/entities.ts
+++ src/types/entities.ts
@@ -13,6 +13,6 @@ export interface User {
     id: number;
     name: string;
     email_verified_at?: string; 
-    created_at: string; 
-    updated_at: string; 
+    created_at?: string; 
+    updated_at?: string; 
 }
